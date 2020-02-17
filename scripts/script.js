87dffd75d4b4893bc7051d997a18a9cda463e108
jwpihlgren <j.pihlgren@outlook.com>
2020-02-17 21:40:14 +0100

    Tweaked remove padding on GIF click

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,14 @@ $("#renter-gif").on("click", (e) => {
     if(e.target.dataset.ison ==="true"){
         e.target.dataset.ison = "false";
         $(e.target).attr("src", "/images/renter_alt.png");
+        $(e.target).removeClass("pt-lg-5");
+        $(e.target).removeClass("pt-md-4");
     }
     else{
         e.target.dataset.ison = "true";
         $(e.target).attr("src", "/images/renter.gif");
+        $(e.target).addClass("pt-lg-5");
+        $(e.target).addClass("pt-md-4");
+        
     }
 });
