2a8ebfa9d7c2e0be183606d54b2c30456caa3457
jwpihlgren <j.pihlgren@outlook.com>
2020-02-17 21:14:28 +0100

    Added new projects

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 $("header .container #nav-wrap").load("/partials/header.html");
+
+$("#renter-gif").on("click", (e) => {
+    if(e.target.dataset.ison ==="true"){
+        e.target.dataset.ison = "false";
+        $(e.target).attr("src", "/images/renter_alt.png");
+    }
+    else{
+        e.target.dataset.ison = "true";
+        $(e.target).attr("src", "/images/renter.gif");
+    }
+});
