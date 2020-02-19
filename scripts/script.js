883ebf2c335f9169293c967fcf6e4d9a96892eb1
jwpihlgren <j.pihlgren@outlook.com>
2020-02-19 19:07:18 +0100

    Added animation and radial overlay to background

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,44 @@
-$("header .container #nav-wrap").load("/partials/header.html");
+const left = $(".left");
+const right = $(".right");
+const nav = $(".top");
+const welcome = $(".welcome");
+const projects = $(".projects");
+console.log(projects);
 
-$("#renter-gif").on("click", (e) => {
-    if(e.target.dataset.ison ==="true"){
-        e.target.dataset.ison = "false";
-        $(e.target).attr("src", "/images/renter_alt.png");
-        $(e.target).removeClass("pt-lg-5");
-        $(e.target).removeClass("pt-md-4");
-    }
-    else{
-        e.target.dataset.ison = "true";
-        $(e.target).attr("src", "/images/renter.gif");
-        $(e.target).addClass("pt-lg-5");
-        $(e.target).addClass("pt-md-4");
-        
-    }
+
+$(document).ready(function () {
+    loadHeaderToPages();
+    runPageIntro();
+    addClickListenerToGif();
+
 });
+
+function loadHeaderToPages(){
+    $("header .container #nav-wrap").load("/partials/header.html");
+}
+
+function addClickListenerToGif(){
+    $("#renter-gif").on("click", (e) => {
+        if(e.target.dataset.ison ==="true"){
+            e.target.dataset.ison = "false";
+            $(e.target).attr("src", "/images/renter_alt.png");
+            $(e.target).removeClass("pt-lg-5");
+            $(e.target).removeClass("pt-md-4");
+        }
+        else{
+            e.target.dataset.ison = "true";
+            $(e.target).attr("src", "/images/renter.gif");
+            $(e.target).addClass("pt-lg-5");
+            $(e.target).addClass("pt-md-4");
+            
+        }
+    });
+}
+
+function runPageIntro() {     
+    $(left).toggleClass("visible");
+    $(right).toggleClass("visible");
+    $(nav).toggleClass("visible");
+    $(welcome).toggleClass("visible");
+    $(projects).toggleClass("visible"); 
+}
